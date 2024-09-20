47b16847d5a1e4afd9ce10b66464ae5858acf644
MarwenSAIDI <marouene.saidi.ds@gmail.com>
2024-09-20 19:04:10 +0100

    Added model configs

--- src/v1/routers/generatorV1_route.py
+++ src/v1/routers/generatorV1_route.py
@@ -2,6 +2,18 @@
 The generator route file
 """
 from fastapi import APIRouter, status
+import os
+from src.v1.utils.loaders import RagChain
+from src.v1.utils.loaders import load_config
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+# load the config file
+config = load_config(os.path.join(os.getcwd(),"config.yml"))
+
+OLLAMA_MODEL_NAME = os.getenv("OLLAMA_MODEL_NAME")
+OLLAMA_URL = os.getenv("OLLAMA_URL")
+CHAIN_CONFIG = config["generater_config"]
 
 router = APIRouter()
 
